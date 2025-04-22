1b506700c354e32be6ed8de5486a348b625d1548
fenroe <scrambles9494@gmail.com>
2025-04-22 11:14:45 +0300

    server running and config changes
    fastify server now runs on port 8080
    changed tsconfig to use nodenext as a compile target
    directed eslint to ignore dist directory

--- wanderlux-be/server.ts
+++ wanderlux-be/server.ts
@@ -1,1 +1,19 @@
-console.log("Hi banana!");
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async (req, res) => {
+  res.send({ message: "Hi banana!" });
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 8080 });
+  } catch (err) {
+    fastify.log.error(err);
+  }
+};
+
+void start();
