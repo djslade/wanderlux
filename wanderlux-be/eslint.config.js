257bc52059ced8b68d2f9204d0442518afa7fe1c
fenroe <scrambles9494@gmail.com>
2025-04-22 14:10:28 +0300

    setup: added prisma to backend
    added prisma as a dependency for backend
    reconfigured eslint to ignore generated files
    expanded scripts to account for generated code from prisma
    added dotenv as a dev dependency
    tested connection to database

--- wanderlux-be/eslint.config.js
+++ wanderlux-be/eslint.config.js
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores(["dist", "generated"]),
   { files: ["**/*.{ts}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{ts}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
--- wanderlux-be/server.ts
+++ wanderlux-be/server.ts
@@ -1,11 +1,22 @@
 import Fastify from "fastify";
+import { PrismaClient } from "./generated/prisma/index.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const fastify = Fastify({
   logger: true,
 });
 
+const prisma = new PrismaClient();
+
 fastify.get("/", async (req, res) => {
   res.send({ message: "Hi banana!" });
+});
+
+fastify.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.send({ users });
 });
 
 const start = async () => {
@@ -13,6 +24,7 @@ const start = async () => {
     await fastify.listen({ port: 8080 });
   } catch (err) {
     fastify.log.error(err);
+    process.exit(1);
   }
 };
 
